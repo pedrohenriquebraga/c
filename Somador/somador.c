7b2d70c1359ad233bdb004a8542b8a5683835490
Pedro Henrique <pedro.braga@wansoft.com.br>
2024-05-23 15:09:05 -0300

    Refactor somador.c to calculate the sum of user input numbers

--- Somador/somador.c
+++ Somador/somador.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {}
+int main()
+{
+
+  int count = 0;
+  int soma = 0;
+
+  while (true)
+  {
+    int novoNum;
+
+    printf("Digite o %d numero (0 para parar): ", count + 1);
+    scanf("%d", &novoNum);
+
+    if (novoNum == 0) {
+      break;
+    }
+
+    soma = soma + novoNum;
+    count++;
+  }
+
+  printf("A soma total dos numeros digitados e %d", soma);
+
+  return 0;
+}
