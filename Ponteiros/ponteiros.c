62d41d1a6a78b79ddf47853061d7dde3576e7be5
Pedro Henrique <pedro.braga@wansoft.com.br>
2024-05-25 14:55:14 -0300

    Add C code for working with pointers

---
+++ Ponteiros/ponteiros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+  int num = 5;
+  int * ptr;
+
+  // Armazenando endereço da váriavel num no ponteiro ptr
+  ptr = &num;
+
+  // Formas de acessar o endereço da váriavel num
+  printf("O endereco da variavel num e %p\n", ptr);
+  printf("O endereco da variavel num e %p\n", &num);
+
+  // Formas de acessar os valores de num
+  printf("O valor de num e %i\n", num);
+  printf("O valor de num e %i\n", *ptr);
+
+  // Alterar o valor de num através do ponteiro
+  *ptr = *ptr + 1;
+
+  printf("Agora o valor de num e %i", num);
+
+  return 0;
+}
